d975134d943d453260f6ff844749916991f8f4f6
Bellonio <bellonegiulios@gmail.com>
2020-10-03 18:05:28 +0200

    app ottimizzata

--- app/src/main/java/com/bellone/catalogofilm/GestoreFile.java
+++ app/src/main/java/com/bellone/catalogofilm/GestoreFile.java
@@ -63,6 +63,44 @@ public class GestoreFile {
 
         //metodo che ritorna una lista con tutti i Film salvati (che serve all'array adapter)
     public ArrayList<Film> readFilms(){
+
+            //HO PROVATO A LEGGERE IL FILE JSON, SALVATO IN res>raw
+
+            //NON MI LEGGE TUTTO IL FILE, ARRIVA FINO AL FILM "Inception" e senza neanche finirlo, ho gia' provato a
+            // leggere riga per riga (usando il BufferedReader) e sono andato su un sito per rimuovere tutti gli spazi/indentazioni...
+            // in eccesso che c'erano ma non basta, non riesce cmq a leggere tutto il file
+        /* InputStream inputStream = getResources().openRawResource(R.raw.all_film_json);
+        String fileJson = "";
+        try {
+            int fileSize = inputStream.available();
+
+                //      PARTE DELLA DOCUMENTAZIONE SUL METODO
+                //Returns an estimate of the number of bytes that can be read (or
+                //    skipped over) from this input stream without blocking by the next
+                //    invocation of a method for this input stream. The next invocation
+                //    might be the same thread or another thread.  A single read or skip of this
+                //    many bytes will not block, but may read or skip fewer bytes.
+
+                //.... infatti lo dice che magari non leggera' l'intero file, ma non ho trovato altri modi ...
+                //Note that while some implementations of {@code InputStream} will return
+                //    the total number of bytes in the stream, many will not.  It is
+                //    never correct to use the return value of this method to allocate
+                //    a buffer intended to hold all data in this stream.
+
+            byte[] buffer = new byte[fileSize];
+
+            inputStream.read(buffer);
+
+            inputStream.close();
+
+            fileJson = new String(buffer, "UTF-8");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            fileJson = null;
+        }
+        Log.d("mieilog", "onResume: "+fileJson); */
+
         File file = new File(files_path+file_name);
         try {
             FileReader fr = new FileReader(file);
